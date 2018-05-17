47fa583db4635b0299b24777a052d46090ce0437
Ibrahim <ibrahimtareq@live.com>
2018-05-17 12:35:46 +1000

    Update APIControllerTest.java

--- src/test/java/com/messagemedia/webhooks/controllers/APIControllerTest.java
+++ src/test/java/com/messagemedia/webhooks/controllers/APIControllerTest.java
@@ -125,6 +125,6 @@ a **Response 400 is returned when** :
 //                 "    {    \"page\": 0,    \"pageSize\": 100,    \"pageData\": [{    \"id\": \"6e2c61df-d30a-4555-82a5-0e79822d8f53\",    \"url\": \"http://myurl.com\",    \"method\": \"POST\",    \"encoding\": \"FORM_ENCODED\",    \"headers\": {    \"Account\": \"FunGuys\"    },    \"template\": \"id=$mtId&status=$statusCode\",    \"events\": [    \"ENROUTE_DR\",    \"DELIVERED_DR\"    ]    }, {    \"id\": \"6e2c61df-d30a-4555-82a5-0e79822d8f53\",    \"url\": \"http://myurl.com\",    \"method\": \"POST\",    \"encoding\": \"XML\",    \"headers\": {    \"Account\": \"FunGuys\"    },    \"template\": \"<content><id> $mtId < /id> <status > $statusCode < /status> </content>\",    \"events\": [    \"ENROUTE_DR\",    \"DELIVERED_DR\"    ]    }]    }", 
 //                 TestHelper.convertStreamToString(httpResponse.getResponse().getRawBody()), 
 //                 true, true, false));
-//     }
+     }
 
 }
